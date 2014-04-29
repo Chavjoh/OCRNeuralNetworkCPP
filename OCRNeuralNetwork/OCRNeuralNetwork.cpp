f1903c5bc48bd40aaeb605ede8991b3835cdf011
Chavjoh <j.chavaillaz@gmail.com>
2014-04-29 13:44:57 +0200

    Create Visual Studio project.

---
+++ OCRNeuralNetwork/OCRNeuralNetwork.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	system("PAUSE");
+	return 0;
+}
+
---
+++ OCRNeuralNetwork/stdafx.cpp
@@ -0,0 +1,6 @@
+// stdafx.cpp : fichier source incluant simplement les fichiers Include standard
+// OCRNeuralNetwork.pch représente l'en-tête précompilé
+// stdafx.obj contient les informations de type précompilées
+
+#include "stdafx.h"
+
